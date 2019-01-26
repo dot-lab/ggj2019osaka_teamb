75e402f677f69837579d51559fb3ad556914165f
Yoshie Takeda <service@dot-lab.xyz>
2019-01-26 14:44:57 +0900

    Update titleScene.js

--- titleScene.js
+++ titleScene.js
@@ -1,53 +1,61 @@
 phina.globalize();
-var family = 0;
-var btnFlag = false;
+
+var family;
+
+phina.define('TitleBtn', {
+  superClass: 'Button',
+  init: function(member,flag) {
+    this.member = member;
+    this.superInit({
+      text: member+'人家族',
+      fill: '#ccc',
+    });
+    this.onpushed = function() {
+      family = member;
+      flag = !flag;
+      switch (flag) { 
+        case true: 
+          fill = 'skyblue';
+          break;
+        case false:
+          fill = '#ccc';
+          break;
+      }
+      console.log(family);
+    }
+    this.onpointend = function() {
+      this.flare('pushed');
+      console.log("!!");
+    }
+  }
+})
+
 // TitleScene
 phina.define('TitleScene', {
   superClass: 'DisplayScene',
   init: function() {
     this.superInit({
       width: SCREEN_WIDTH,
-      height: SCREEN_HEIGHT
+      height: SCREEN_HEIGHT,
     });
     // タイトル
     Label({
       text: '家族の離し合い - Family Talking!'
     }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
 
-    var btn_3 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(6))
-    btn_3.text = '3人家族';
-    btn_3.onpointend = function() {
-      family = 3;
-      btnFlag = true;
-      console.log(family);
-    }
-    var btn_4 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(9))
-    btn_4.text = '4人家族';
-    btn_4.onpointend = function() {
-      family = 4;
-      btnFlag = true;
-      console.log(family);
-    }
-    var btn_6 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(12))
-    btn_6.text = '6人家族';
-    btn_6.onpointend = function() {
-      family = 6;
-      btnFlag = true;
-      console.log(family);
-    }
+    var btn_3 = TitleBtn(3,false).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(6))
+    var btn_4 = TitleBtn(4,false).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(9))
+    var btn_6 = TitleBtn(6,false).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(12))
 
     var startBtn = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(15))
     startBtn.text = "TALK START";
     startBtn.width = 400;
+    startBtn.fill = 'red';
     startBtn.onpointend = function() {
-      if ( btnFlag ) {
-        var scene = this.getParent();
-        scene.exit({
-          pushBtn: family,
-        });
-      } else {
-        alert("家族構成を選択してね！");
-      }
+      var scene = this.getParent();
+      scene.exit({
+        pushBtn: family,
+      });
     }
   },
 });
