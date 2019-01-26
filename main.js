779bec48c0312c3eef12e5ab543bb6ba7e81ea0f
Yoshie Takeda <service@dot-lab.xyz>
2019-01-26 13:17:16 +0900

    scene遷移

--- main.js
+++ main.js
@@ -1,25 +1,37 @@
 // MainScene クラスを定義
 phina.define('MainScene', {
   superClass: 'DisplayScene',
-  init: function() {
+  init: function(param) {
+    this.superInit(param);
     this.superInit({
       width: SCREEN_WIDTH,
-      height: SCREEN_HEIGHT
+      height: SCREEN_HEIGHT,
+      backgroundColor: 'blue',
     });
-    // 背景色を指定
-    this.backgroundColor = '#444';
-    // ラベルを生成
-    this.label = Label('Hello, phina.js!').addChildTo(this);
-    this.label.x = this.gridX.center(); // x 座標
-    this.label.y = this.gridY.center(); // y 座標
-    this.label.fill = 'white'; // 塗りつぶし色
+    Label({
+      text: param.pushBtn,
+    })
+    .addChildTo(this)
+    .setPosition(this.gridX.center(), this.gridY.center())
   },
 });
 
 // メイン処理
 phina.main(function() {
-  // アプリケーション生成
   var app = GameApp({
+    startLabel: 'title',
+    scenes: [
+      {
+        className: 'TitleScene',
+        label: 'title',
+        nextLabel: 'play',
+      },
+      {
+        className: 'MainScene',
+        label: 'play',
+        nextLabel: 'title',
+      },
+    ],
     width: SCREEN_WIDTH,
     height: SCREEN_HEIGHT
   });
--- titleScene.js
+++ titleScene.js
@@ -1,4 +1,6 @@
 phina.globalize();
+var family = 0;
+var btnFlag = false;
 // TitleScene
 phina.define('TitleScene', {
   superClass: 'DisplayScene',
@@ -12,15 +14,37 @@ phina.define('TitleScene', {
       text: '家族の離し合い - Family Talking!'
     }).addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(4));
 
-    Label({
-      text: "START",
-      fontSize: 32,
-    }).addChildTo(this)
-      .setPosition(this.gridX.center(), this.gridY.span(12))
-    // 画面タッチ時
-    this.on('pointend', function() {
-      // 次のシーンへ
-      this.exit();
-    });
-  }
+    var btn_3 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(6))
+    btn_3.text = '3人家族';
+    btn_3.onpointend = function() {
+      family = 3;
+      btnFlag = true;
+      console.log(family);
+    }
+    var btn_4 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(9))
+    btn_4.text = '4人家族';
+    btn_4.onpointend = function() {
+      family = 4;
+      btnFlag = true;
+      console.log(family);
+    }
+    var btn_6 = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(12))
+    btn_6.text = '6人家族';
+    btn_6.onpointend = function() {
+      family = 6;
+      btnFlag = true;
+      console.log(family);
+    }
+
+    var startBtn = Button().addChildTo(this).setPosition(this.gridX.center(), this.gridY.span(15))
+    startBtn.text = "TALK START";
+    startBtn.width = 400;
+    startBtn.onpointend = function() {
+      console.log("!");
+      var scene = this.getParent();
+      scene.exit({
+        pushBtn: family,
+      });
+    }
+  },
 });
